4fe7b77a46385aeb6f670136bcba2652a46a0a51
Frog Chen <satgo1546@yeah.net>
2026-08-26 20:17:56 +0800

    【！！！】【！！！】

--- src/Python/sample.py
+++ src/Python/sample.py
@@ -18,10 +18,7 @@ def my_callback(msg, details, arg):
 
 if __name__ == "__main__":
     # 请设置为存放 dll 文件及资源的路径
-    path = pathlib.Path(__file__).resolve().parent.parent
-
-    # 设置更新器的路径和目标版本并更新
-    Updater(path, Version.Stable).update()
+    path = pathlib.Path(__file__).resolve().parent.parent.parent
 
     # 加载主资源及增量副资源
     #
@@ -57,7 +54,7 @@ if __name__ == "__main__":
 
     # 设置额外配置
     # 触控方案配置
-    asst.set_instance_option(InstanceOptionType.touch_type, "maatouch")
+    asst.set_instance_option(InstanceOptionType.touch_type, "Adb")
     # 暂停下干员
     # asst.set_instance_option(InstanceOptionType.deployment_with_pause, '1')
 
@@ -68,7 +65,7 @@ if __name__ == "__main__":
     # port = Bluestacks.get_hyperv_port(r"C:\ProgramData\BlueStacks_nxt\bluestacks.conf", "Pie64_1")
 
     # 请自行配置 adb 环境变量，或修改为 adb 可执行程序的路径
-    if asst.connect("adb.exe", "127.0.0.1:5555"):
+    if asst.connect("/usr/bin/adb", "127.0.0.1:5555"):
         print("连接成功")
     else:
         print("连接失败")
